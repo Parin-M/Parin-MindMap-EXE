1051d72dab000e5cc57bad896d684a643c517096
Parin.M <324813304+Parin-M@users.noreply.github.com>
2026-09-18 15:46:57 +0330

    feat: expose AI gateway to renderer

--- electron/preload.cjs
+++ electron/preload.cjs
@@ -4,6 +4,7 @@ contextBridge.exposeInMainWorld("parinAPI", {
   saveMindMap: (data) => ipcRenderer.invoke("dialog:save-json", data),
   openMindMap: () => ipcRenderer.invoke("dialog:open-json"),
   saveText: (data) => ipcRenderer.invoke("dialog:save-text", data),
+  aiRequest: (data) => ipcRenderer.invoke("ai:request", data),
   exportPDF: (data) => ipcRenderer.invoke("pdf:export", data),
   openExternal: (url) => ipcRenderer.invoke("app:open-external", url)
 });
